ab31671c57d02cce6e96552d7c724356f65e1cac
Victor Luiz <c0007043@vli-logistica.com.br>
2022-10-29 17:18:03 -0300

    Criando controller usuário

---
+++ MC.MedicalClinic/MC.MedicalClinic/Controllers/Usuario/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MC.MedicalClinic.Controllers.Usuario
+{
+    [ApiController]
+    [Route("usuario")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet(Name = "VerificarUsuario")]
+        public int GetUser()
+        {
+            return 0;
+        }
+
+        [HttpGet("cadastrarUsuario")]
+        public IActionResult GetCadastrarUsuario()
+        {
+            return Ok("Cadastrado com sucesso");
+        }
+    }
+}
